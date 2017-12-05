69aab319192090aece3f262f9f3a9696821ee2a3
Alvin Muala <alvin@driverevenue.net>
2017-12-05 21:37:02 +0200

    Beginner Series #1 School Paperwork

---
+++ 8 Kyu/Beginner Series #1 School Paperwork.cs
@@ -0,0 +1,10 @@
+// Beginner Series #1 School Paperwork
+
+ using System;
+  public static class Paper
+  {
+    public static int Paperwork(int n, int m)
+    {
+       return m < 0 ? 0 : n < 0 ? 0 : m * n;
+    }
+  }
